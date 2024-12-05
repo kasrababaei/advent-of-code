80bc2a8630a1431297bbc9bde315e0ac460af05f
Kasra Babaei <kasrababaei@gmail.com>
2024-12-04 23:45:26 -0700

    Y2024Day05

--- Sources/2024/Y2024Day05.swift
+++ Sources/2024/Y2024Day05.swift
@@ -5,10 +5,85 @@ struct Y2024Day05: AdventDay {
   let data: String
   
   func part1() throws -> Any {
-    return 0
+    let (rules, pages) = try input()
+    
+    var sum = 0
+    for page in pages {
+      var isValid = true
+      for index in (0..<page.count).reversed() where isValid {
+        let precedingRules = page[0..<index]
+        guard let forbiddenYs = rules[page[index]] else { continue }
+        isValid = Set(forbiddenYs).intersection(precedingRules).isEmpty
+      }
+      
+      if isValid {
+        sum += page[page.count / 2]
+      }
+    }
+    
+    return sum
   }
   
   func part2() throws -> Any {
-    return 0
+    let (rules, pages) = try input()
+    
+    var invalidPages: [[Int]] = []
+    var sum = 0
+    
+    for page in pages {
+      var isValid = true
+      for index in (0..<page.count).reversed() where isValid {
+        let precedingRules = page[0..<index]
+        guard let forbiddenYs = rules[page[index]] else { continue }
+        isValid = Set(forbiddenYs).intersection(precedingRules).isEmpty
+      }
+      
+      if !isValid {
+        invalidPages.append(page)
+      }
+    }
+    
+    for page in invalidPages {
+      var mutatingPage = page
+      var index = mutatingPage.count - 1
+      
+      while index >= 0 {
+        let precedingRules = mutatingPage[0..<index]
+        guard let forbiddenYs = rules[mutatingPage[index]],
+              !Set(forbiddenYs).intersection(precedingRules).isEmpty
+        else {
+          index -= 1
+          continue
+        }
+        
+        let lastIndex = precedingRules.lastIndex(where: { forbiddenYs.contains($0) })!
+        mutatingPage.swapAt(index, lastIndex)
+        index = mutatingPage.count - 1
+      }
+      
+      sum += mutatingPage[mutatingPage.count / 2]
+    }
+    
+    return sum
+  }
+  
+  private func input() throws -> (rules: [Int: [Int]], pages: [[Int]]) {
+    let split = data.split(separator: "\n\n")
+    let rules = try split[0]
+      .split(separator: "\n")
+      .map { line in
+        let x = try line.split(separator: "|")[0].toInteger()
+        let y = try line.split(separator: "|")[1].toInteger()
+        return (x, y)
+      }
+      .reduce(into: [Int: [Int]]()) { $0[$1.0, default: []].append($1.1) }
+    
+    let pages = try split[1]
+      .split(separator: "\n")
+      .map { line in
+        try line.split(separator: ",").map { try $0.toInteger() }
+      }
+    
+    return (rules, pages)
   }
 }
--- Tests/2024/Y2024Day05Tests.swift
+++ Tests/2024/Y2024Day05Tests.swift
@@ -2,18 +2,51 @@ import Testing
 @testable import AdventOfCode
 
 struct Y2024Day05Tests {
-  let testData = ""
+  let testData = """
+  47|53
+  97|13
+  97|61
+  97|47
+  75|29
+  61|13
+  75|53
+  29|13
+  97|29
+  53|29
+  61|53
+  97|53
+  61|29
+  47|13
+  75|47
+  97|75
+  47|61
+  75|61
+  47|29
+  75|13
+  53|13
+
+  75,47,61,53,29
+  97,61,53,29,13
+  75,29,13
+  75,97,47,61,53
+  61,13,29
+  97,13,75,29,47
+  """
     
   @Test func testPart1() async throws {
-    let testData = ""
-    #expect(try Y2024Day05(data: testData).part1() as! Int == 0)
-    #expect(try Y2024Day05().part1() as! Int == 0)
+    let testResult = try Y2024Day05(data: testData).part1() as! Int
+    #expect(testResult == 143)
+    
+    let inputResult = try Y2024Day05().part1() as! Int
+    #expect(inputResult == 5509)
   }
   
   @Test func testPart2() async throws {
-    let testData = ""
-    #expect(try Y2024Day05(data: testData).part2() as! Int == 0)
-    #expect(try Y2024Day05().part2() as! Int == 0)
+    let testResult = try Y2024Day05(data: testData).part2() as! Int
+    #expect(testResult == 123)
+    
+    let inputResult = try Y2024Day05().part2() as! Int
+    #expect(inputResult == 4407)
   }
 }
 
