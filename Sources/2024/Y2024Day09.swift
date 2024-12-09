81441b14dbc2bf08ee45df0f4bf3fbe49e71f85a
Kasra Babaei <kasrababaei@gmail.com>
2024-12-09 15:59:47 -0700

    Y2024Day09

--- Sources/2024/Y2024Day09.swift
+++ Sources/2024/Y2024Day09.swift
@@ -5,6 +5,12 @@ struct Y2024Day09: AdventDay {
   let data: String
   
   func part1() throws -> Any {
+    let diskMap = try diskMap()
+    
+    var fileBlockIndex = diskMap.endIndex
+    var freeSpaceIndex = diskMap.startIndex
+    
+    
     return 0
   }
   
@@ -12,3 +18,39 @@ struct Y2024Day09: AdventDay {
     return 0
   }
 }
+
+private extension Y2024Day09 {
+  func diskMap() throws -> String {
+    let digits = try Array(data).map { try $0.toInteger() }
+    var diskMap = ""
+    
+    var firstFreeSpaceIndex: String.Index?
+    var lastFileSpaceIndex: String.Index?
+    
+    for index in stride(from: 0, to: digits.count, by: 2) {
+      diskMap += String(repeating: "\(index / 2)", count: digits[index])
+      if digits.indices.contains(index + 1) {
+        diskMap += String(repeating: ".", count: digits[index + 1])
+      }
+    }
+    
+    return diskMap
+  }
+  
+  struct DiskMap {
+    let string: String
+    let firstFreeSpaceIndex: String.Index
+    let lastFileSpaceIndex: String.Index
+    
+    init(
+      _ string: String,
+      _ firstFreeSpaceIndex: String.Index,
+      _ lastFileSpaceIndex: String.Index
+    ) {
+      self.string = string
+      self.firstFreeSpaceIndex = firstFreeSpaceIndex
+      self.lastFileSpaceIndex = lastFileSpaceIndex
+    }
+  }
+}
+
--- Tests/2024/Y2024Day09Tests.swift
+++ Tests/2024/Y2024Day09Tests.swift
@@ -3,20 +3,30 @@ import Testing
 
 struct Y2024Day09Tests {
   let testData = """
+  2333133121414131402
   """
     
   @Test func testPart1() async throws {
     let testData = """
+    12345
     """
-    #expect(try Y2024Day09(data: testData).part1() as! Int == 0)
-    #expect(try Y2024Day09().part1() as! Int == 0)
+    
+    let testResult = try Y2024Day09(data: testData).part1() as! Int
+    #expect(testResult == 1928)
+    
+    let inputResult = try Y2024Day09().part1() as! Int
+    #expect(inputResult == 0)
   }
   
   @Test func testPart2() async throws {
     let testData = """
     """
-    #expect(try Y2024Day09(data: testData).part2() as! Int == 0)
-    #expect(try Y2024Day09().part2() as! Int == 0)
+    
+    let testResult = try Y2024Day09(data: testData).part2() as! Int
+    #expect(testResult == 0)
+    
+    let inputResult = try Y2024Day09().part2() as! Int
+    #expect(inputResult == 0)
   }
 }
 
